d38fcb3133b6f9ec4b5e3da258843deeb283fab1
mesic <mesic@MacBook-Pro-de-mesic.local>
2020-10-19 19:51:30 +0200

    header-position-font-and-color

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,12 @@
 import React from "react"
 import "../styles/styles.scss"
 
+//Import Components
+import Header from "../components/header"
 const IndexPage = () => (
-  <p>Home</p>
-  
+  <Header /> 
 )
+  
+
 
 export default IndexPage
